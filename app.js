e40cc86eb4363613eb925f6247e598380ca2f558
Fcuervo21 <fernando.cuervola@udlap.mx>
2022-01-31 21:53:40 -0600

    Folder structure 2

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3030);
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public")); // Para colocar la carpeta public con css, img
+app.use(express.json());
+
+
+app.get('/', function(req, res){
+    res.send('Hola')
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log("Server started on port", app.get('port'));
+});
